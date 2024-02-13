68057b51d6f4e2ed53a4e006932f40b11207f30d
Umer siddique <sheikhumersiddique1999@gmail.com>
2024-02-13 23:55:26 +0500

    rtk crud

--- src/services/customersApi.jsx
+++ src/services/customersApi.jsx
@@ -4,10 +4,44 @@ export const customersApi = createApi({
   reducerPath: "customersApi",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8000/api/v1/" }),
   endpoints: (builder) => ({
+    // Get All Customers
     customers: builder.query({
       query: () => "/customers",
+    }),
+    // Get one customer
+    customer: builder.query({
+      query: (id) => `/customers/${id}`,
+    }),
+    // Create customer
+    createCustomer: builder.mutation({
+      query: (customer) => ({
+        url: "/customers",
+        method: "POST",
+        body: customer,
+      }),
+    }),
+    // Update Customer
+    updateCustomer: builder.mutation({
+      query: ({ id, ...rest }) => ({
+        url: `/customers/${id}`,
+        method: "PATCH",
+        body: rest,
+      }),
+    }),
+    // Delete Customer
+    deleteCustomer: builder.mutation({
+      query: (id) => ({
+        url: `/customers/${id}`,
+        method: "Delete",
+      }),
     }),
   }),
 });
 
-export const { useCustomersQuery } = customersApi;
+export const {
+  useCustomersQuery,
+  useCustomerQuery,
+  useCreateCustomerMutation,
+  useUpdateCustomerMutation,
+  useDeleteCustomerMutation,
+} = customersApi;
